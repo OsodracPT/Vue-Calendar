85207e5a79f8003a296587099721ae3a4a23468c
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-10-15 16:45:35 +0100

    Calendar component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+var CONFIG = require("./config");
+
+// Initialize Firebase
+firebase.initializeApp(CONFIG.firebaseConfig);
+
+export const db = firebase.firestore();
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
